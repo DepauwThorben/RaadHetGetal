91f0cf926323d0dfee6a17044bf448fc6015b34b
DepauwThorben <depauwthorbengmail.com>
2021-01-15 01:10:27 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  deviation: number;
+  noOfTries: number;
+  original: number;
+  guess: number;
+  
+
+  constructor() {
+   
+    this.initializeGame();    
+  }
+  initializeGame() {
+    this.noOfTries = 10;
+    this.original = Math.floor(Math.random() * 100 + 1);
+    this.guess = null;
+    this.deviation = null;
+   
+  }
+  verifyGuess() {
+    this.deviation = this.original - this.guess;
+    this.noOfTries = this.noOfTries - 1;
+   
+  }
 }
